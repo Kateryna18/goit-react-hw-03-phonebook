af00a8fce9f3b7bab9bbb4c6ce2e55c4894e911e
Kateryna18 <mikolaichukkate@icloud.com>
2023-05-03 15:59:33 +0300

    added base arhitect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,66 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { ContactForm } from './ContactForm/ContactForm';
+
+import React, { Component } from 'react';
+import { ContactsList } from './ContactsList/ContactsList';
+import { Filter } from './Filter/Filter';
+import toast, { Toaster } from 'react-hot-toast';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  handleAddContact = contact => {
+    if (this.state.contacts.some(item => item.name === contact.name)) {
+      toast.error('Contact already exists');
+      return true;
+    }
+    this.setState(prevState => {
+      return {
+        contacts: [...prevState.contacts, contact],
+      };
+    });
+    return false;
+  };
+
+  handleDeleteContact = id => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(contact => contact.id !== id),
+      };
+    });
+  };
+
+  handleChangeFilter = e => {
+    this.setState({ filter: e.target.value });
+  };
+
+  handleFilterContacts = () => {
+    return this.state.contacts.filter(contact =>
+      contact.name
+        .toLowerCase()
+        .includes(this.state.filter.toLowerCase().trim())
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm addContact={this.handleAddContact} />
+        <h2>Contacts</h2>
+        <Filter
+          value={this.state.filter}
+          handleChange={this.handleChangeFilter}
+        />
+        {this.state.contacts.length !== 0 && <ContactsList
+          contacts={this.handleFilterContacts()}
+          deleteContact={this.handleDeleteContact}
+        />}
+        
+        <Toaster />
+      </div>
+    );
+  }
+}
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import css from 'components/ContactForm/ContactForm.module.css';
+
+export class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const isContactExists = this.props.addContact({
+      id: nanoid(6),
+      ...this.state,
+    });
+    if (!isContactExists) {
+      this.reset();
+    }
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+    
+  };
+
+  reset = () => {
+    this.setState({
+      name: '',
+      number: '',
+    });
+  };
+
+  render() {
+    return (
+      <form className={css.form} onSubmit={this.handleSubmit}>
+        <div className={css.formLabelBox}>
+          <label className={css.formLabel}>
+            Name <input
+              className={css.formInput}
+              onChange={this.handleChange}
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              value={this.state.name}
+            />
+          </label>
+          <label className={css.formLabel}>
+            Number <input
+              className={css.formInput}
+              onChange={this.handleChange}
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+              value={this.state.number}
+            />
+          </label>
+        </div>
+        <button className={css.formButton} type="submit">Add Contact</button>
+      </form>
+    );
+  }
+}
